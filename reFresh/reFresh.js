83e637623f31eabe6cdb52f4ad1165263410ecea
unFearing <chen240sx@gmail.com>
2015-10-17 17:00:01 -0500

    updated files

--- reFresh/reFresh.js
+++ reFresh/reFresh.js
@@ -1,23 +1,62 @@
+//var namespace = {'Computer Science', 'News', 'Business', 'UrbanDictionary'};
+var namespace = {};
+namespace['Computer Science'] = {};
+namespace['Computer Science']['techcrunch.com'] = "";
+namespace['Computer Science']['wikipedia.org'] = "";
+console.log("Poop");
+search = function(query){
+  var total = 0;
+  var artCount = 0;
+  var theQuery = namespace[query];
+  for (var dom in theQuery ){
+    //dom = {}; //declare array for articles
+    while (artCount < 3){
+      var site = "site:" + dom;
+      var fdate = new Date().getFullYear();
+      var idate = fdate - 1;
+      var range = idate + ".." + fdate;
+      var fire = site + " " + query + " " + range;
+      console.log(fire);
+
+      //grab url of specific article
+
+      //dom.push(/*url*/);
+
+      artCount++;
+      total++;  
+    }
+    artCount = 0; /*reset */
+  }
+};
+
+
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
+  // This code only runs on the client
+  Template.body.helpers({
+    tasks: function () {
+      // Show newest tasks at the top
+      return Tasks.find({}, {sort: {createdAt: -1}});
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+  Template.body.events({
+    "submit .new-task": function (event) {
+      // Prevent default browser form submit
+      event.preventDefault();
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+      // Get value from form element
+      var text = event.target.text.value;
+
+      // Insert a task into the collection
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.text.value = "";
+    }
   });
 }
