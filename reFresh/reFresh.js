8b91b76cd4930ee5097438b3fb84fd57307d8433
rplanek2 <rlplanek@gmail.com>
2015-10-18 01:30:44 -0500

    blah
    blH

--- reFresh/reFresh.js
+++ reFresh/reFresh.js
@@ -1,18 +1,89 @@
+//ROUTER TO HOME TEMPLATE
+Router.route('/', function () {
+  this.render('Home');
+});
+
+//ROUTERS TO ARTICLE TEMPLATES
+Router.route('/ComputerScience');
+Router.route('/Business');
+Router.route('/News');
+Router.route('/nrop');
+Router.route('/UrbanDictionary');
+
+if (Meteor.isClient) {
+
+  //SETS TITLE OF BUTTONS
+  Template.home.helpers({
+    buttons: [
+      {title: "ComputerScience"},
+      {title: "Business"},
+      {title: "News"},
+      {title: "nrop"},
+      {title: "UrbanDictionary"}
+    ]
+  });
+
+  //ACTION LISTENER FOR BUTTONS
+  Template.button.events({
+    'click .start': function () {
+      Router.go('/' + this.title);
+    }
+  });
+
+  Template.ComputerScience.onCreated(){  
+    //console.log(this);
+    var x = Template.instance();
+
+    console.log(x.name);
+
+    console.log(x.innerHTML);
+    
+  });
+  Template.Business.helpers({
+
+  });
+  Template.News.helpers({
+
+  });
+  Template.nrop.helpers({
+
+  });
+  Template.UrbanDictionary.helpers({
+
+  });
 
 
 
 
-//var namespace = {'Computer Science', 'News', 'Business', 'UrbanDictionary'};
-/*
-var namespace = {};
-namespace['Computer Science'] = {};
-namespace['Computer Science']['techcrunch.com'] = "";
-namespace['Computer Science']['wikipedia.org'] = "";
-console.log("Poop");
+
+
+
+
+
+
+};
+
+var Test = function() {
+  var unorderedList = document.getElementById('myList');
+  var listItems = unorderedList.getElementsByTagName('li');
+
+  for (var button, i = 0; i < listItems.length; i++){
+    console.log(button, i);
+  }
+}
+
+var ComputerScience[5];
+var Business[5];
+var News[5];
+var nrop[5];
+var UrbanDictionary = "http://www.urbandictionary.com/";
+
+var dom = {ComputerScience, Business, News, nrop, UrbanDictionary};
+
 search = function(query){
   var total = 0;
   var artCount = 0;
-  var theQuery = namespace[query];
+  var theQuery = dom[query];
   for (var dom in theQuery ){
     //dom = {}; //declare array for articles
     while (artCount < 3){
@@ -30,47 +101,6 @@ search = function(query){
       artCount++;
       total++;  
     }
-    artCount = 0; /*reset 
+    artCount = 0;
   }
 };
-
-*/
-Tasks = new Mongo.Collection("tasks");
-
-Router.route('/register');
-
-Router.route('/', function () {
-  this.render('Home');
-});
-if (Meteor.isClient) {
-
-  Template.home.helpers({
-    buttons: [
-      {title: "Computer Science"},
-      {title: "Business"},
-      {title: "News"},
-      {title: "nrop"},
-      {title: "YouTube"}
-    ]
-  });
-
-  Template.button.events({
-    'click .start': function () {
-      Test();
-      //Router.go('/register');
-    }
-  });
-};
-
-var Test = function() {
-  var unorderedList = document.getElementById('myList');
-  var listItems = unorderedList.getElementsByTagName('li');
-
-  for (var button, i = 0; i < listItems.length; i++){
-    console.log(button, i);
-  }
-}
-
-
-
-
